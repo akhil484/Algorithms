950c55708153d05023776b9260da9ad78ae2775c
rahulmishra11 <rahulmishradbg123@gmail.com>
2020-07-28 02:17:48 +0530

    Changes in MyTemplate

--- myTemplate.cpp
+++ myTemplate.cpp
@@ -282,26 +282,3 @@ struct Comp {
 */
 
 
-void leftrotate(string &s, int d)
-{
-    reverse(s.begin(), s.begin() + d);
-    reverse(s.begin() + d, s.end());
-    reverse(s.begin(), s.end());
-}
-
-void rightrotate(string &s, int d)
-{
-    leftrotate(s, s.length() - d);
-}
-
-
-
-
-
-int main()
-{
-    FIFO;
-
-
-    return 0;
-}
